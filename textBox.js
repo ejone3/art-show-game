057c21ff38fdd9a136460fcbfa7803b1092d6cb7
ejone3 <76170430+ejone3@users.noreply.github.com>
2021-01-25 19:40:35 -0500

    Add a subclass
    I added a subclass to "TextBox" to make textbooks that had a blue border instead of a red one to show a distinction between speaking to someone and collecting something.

--- textBox.js
+++ textBox.js
@@ -1,4 +1,4 @@
-class TextBox{
+class TextBox {
   constructor(message) {
     this.x = 100;
     this.y = 200;
@@ -12,7 +12,25 @@ class TextBox{
   }
   
   show() {
-    fill(201, 48, 48);
+    fill(255, 56, 56);
+    rect(this.x - 20, this.y - 20, this.width + 40, this.height + 40, 20);
+    fill(255, 253, 196);
+    rect(this.x, this.y, this.width, this.height, 20);
+    textSize(20);
+    textAlign(CENTER);
+    fill(0);
+    text(this.message, this.x + this.width/2, this.y + this.height/1.7);
+    text(this.message2, this.x + this.width/2, this.y + this.height/1.4);
+  }
+}
+
+class CollectionBox extends TextBox {
+  constructor(message) {
+    super(message);
+  }
+  
+  show() {
+    fill(56, 76, 255);
     rect(this.x - 20, this.y - 20, this.width + 40, this.height + 40, 20);
     fill(255, 253, 196);
     rect(this.x, this.y, this.width, this.height, 20);
